f0018cc2433ea6dab9aa00cf8bec60a28029e220
jomagsm <jomagsm@gmail.com>
2021-05-26 22:55:56 +0600

    remade CharacterList and CharacterGrid in Statless

--- lib/screens/character_list/widgets/characters_grid_view.dart
+++ lib/screens/character_list/widgets/characters_grid_view.dart
@@ -3,19 +3,11 @@ import 'package:rick_and_morty/global/navigator.dart';
 import 'package:rick_and_morty/screens/character_profile/screen.dart';
 import 'package:rick_and_morty/theme/text_theme.dart';
 
-class CharactersGridView extends StatefulWidget {
+class CharactersGridView extends StatelessWidget {
   final List charactersList;
-  CharactersGridView({Key key, @required this.charactersList})
+  const CharactersGridView({Key key, @required this.charactersList})
       : super(key: key);
 
-  @override
-  _CharactersGridViewState createState() =>
-      _CharactersGridViewState(charactersList);
-}
-
-class _CharactersGridViewState extends State<CharactersGridView> {
-  List charactersList;
-  _CharactersGridViewState(this.charactersList);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -34,13 +26,6 @@ class _CharactersGridViewState extends State<CharactersGridView> {
                     context,
                     SlideRightRoute(
                         page: CharacterProfile(id: charactersList[index].id)));
-                // Navigator.push(
-                //   context,
-                //   MaterialPageRoute(
-                //     builder: (context) =>
-                //         CharacterProfile(id: charactersList[index].id),
-                //   ),
-                // );
               },
               child: Column(
                 children: [
@@ -79,3 +64,80 @@ class _CharactersGridViewState extends State<CharactersGridView> {
     );
   }
 }
+
+// class CharactersGridView extends StatefulWidget {
+//   final List charactersList;
+//   CharactersGridView({Key key, @required this.charactersList})
+//       : super(key: key);
+
+//   @override
+//   _CharactersGridViewState createState() =>
+//       _CharactersGridViewState(charactersList);
+// }
+
+// class _CharactersGridViewState extends State<CharactersGridView> {
+//   List charactersList;
+//   _CharactersGridViewState(this.charactersList);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       margin: EdgeInsets.only(top: 28),
+//       child: GridView.builder(
+//           physics: NeverScrollableScrollPhysics(),
+//           scrollDirection: Axis.vertical,
+//           shrinkWrap: true,
+//           itemCount: charactersList.length,
+//           gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+//               crossAxisCount: 2, mainAxisSpacing: 24),
+//           itemBuilder: (_, index) {
+//             return InkWell(
+//               onTap: () {
+//                 Navigator.push(
+//                     context,
+//                     SlideRightRoute(
+//                         page: CharacterProfile(id: charactersList[index].id)));
+//                 // Navigator.push(
+//                 //   context,
+//                 //   MaterialPageRoute(
+//                 //     builder: (context) =>
+//                 //         CharacterProfile(id: charactersList[index].id),
+//                 //   ),
+//                 // );
+//               },
+//               child: Column(
+//                 children: [
+//                   Container(
+//                     width: 120,
+//                     height: 122,
+//                     child: CircleAvatar(
+//                         radius: 50,
+//                         child: Image.asset(
+//                           charactersList[index].avatar,
+//                         )),
+//                   ),
+//                   Container(
+//                     margin: EdgeInsets.only(top: 18),
+//                     height: 55,
+//                     child: Column(
+//                       mainAxisAlignment: MainAxisAlignment.spaceAround,
+//                       children: [
+//                         Text(charactersList[index].status.toUpperCase(),
+//                             style: getTextTheme(charactersList[index].status)),
+//                         Text(
+//                           "${charactersList[index].firstName} ${charactersList[index].lastName}",
+//                           style: TextThemes.fullNameBigCard,
+//                         ),
+//                         Text(
+//                           "${charactersList[index].race} ${charactersList[index].gender}",
+//                           style: TextThemes.textAppearanceCaption,
+//                         )
+//                       ],
+//                     ),
+//                   ),
+//                 ],
+//               ),
+//             );
+//           }),
+//     );
+//   }
+// }
--- lib/screens/character_list/widgets/characters_listView.dart
+++ lib/screens/character_list/widgets/characters_listView.dart
@@ -1,21 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:rick_and_morty/global/navigator.dart';
 import 'package:rick_and_morty/screens/character_profile/screen.dart';
+import 'package:rick_and_morty/screens/models/character_model.dart';
 import 'package:rick_and_morty/theme/text_theme.dart';
 
-class CharactersListView extends StatefulWidget {
-  final List charactersList;
-  CharactersListView({Key key, @required this.charactersList})
+class CharactersListView extends StatelessWidget {
+  final List<Character> charactersList;
+  const CharactersListView({Key key, @required this.charactersList})
       : super(key: key);
 
-  @override
-  _CharactersListViewState createState() =>
-      _CharactersListViewState(charactersList);
-}
-
-class _CharactersListViewState extends State<CharactersListView> {
-  List charactersList;
-  _CharactersListViewState(this.charactersList);
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -73,3 +66,74 @@ class _CharactersListViewState extends State<CharactersListView> {
     ));
   }
 }
+
+// class CharactersListView extends StatefulWidget {
+//   final List charactersList;
+//   CharactersListView({Key key, @required this.charactersList})
+//       : super(key: key);
+
+//   @override
+//   _CharactersListViewState createState() =>
+//       _CharactersListViewState(charactersList);
+// }
+
+// class _CharactersListViewState extends State<CharactersListView> {
+//   List charactersList;
+//   _CharactersListViewState(this.charactersList);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//         child: ListView.builder(
+//       physics: NeverScrollableScrollPhysics(),
+//       scrollDirection: Axis.vertical,
+//       shrinkWrap: true,
+//       itemCount: charactersList.length,
+//       itemBuilder: (_, index) {
+//         return InkWell(
+//           onTap: () {
+//             Navigator.push(
+//                 context,
+//                 SlideRightRoute(
+//                     page: CharacterProfile(id: charactersList[index].id)));
+//           },
+//           child: Container(
+//             margin: EdgeInsets.only(top: 24, left: 16),
+//             child: Row(
+//               children: [
+//                 Container(
+//                   margin: EdgeInsets.only(right: 18),
+//                   width: 74,
+//                   height: 74,
+//                   child: CircleAvatar(
+//                     radius: 50,
+//                     child: Image.asset(
+//                       charactersList[index].avatar,
+//                     ),
+//                   ),
+//                 ),
+//                 Column(
+//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                   crossAxisAlignment: CrossAxisAlignment.start,
+//                   children: [
+//                     Text(
+//                       "${charactersList[index].status.toUpperCase()}",
+//                       style: getTextTheme(charactersList[index].status),
+//                     ),
+//                     Text(
+//                       "${charactersList[index].firstName} ${charactersList[index].lastName}",
+//                       style: TextThemes.textAppearanceOverlineFullName,
+//                     ),
+//                     Text(
+//                       "${charactersList[index].race} ${charactersList[index].gender}",
+//                       style: TextThemes.textAppearanceCaption,
+//                     )
+//                   ],
+//                 )
+//               ],
+//             ),
+//           ),
+//         );
+//       },
+//     ));
+//   }
+// }
